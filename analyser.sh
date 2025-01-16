127fd60e2a8ce22f945f0b929e069b546147e609
Pheblar <masterrat511@gmail.com>
2025-01-16 21:50:53 +0200

    feat: add files and 1st step

---
+++ analyser.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+LOG_FILE="nginx-access.log"
+
+if [ ! -f $LOG_FILE ]; then  
+    echo "Log file $LOG_FILE not found!"
+    exit 1  
+fi
+
+echo "Top 5 IP addresses with the most requests:"
+grep -o '^[^ ]*' $LOG_FILE | sort | uniq -c | sort -nr | head -n 5
